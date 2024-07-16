882418fc42fdbf77d81906ce75fd9dbd67c4cc95
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-16 10:41:55 -0700

    adding state to the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react'
 import './App.css'
 
+
+
 export default function App() {
+
+
+
+  const [dateState, setDateState] = useState(69)
+  const [priceState, setPriceState] = useState(3.69)
+  const [gallonState, setGallonState] = useState(14)
+
+
+
   return (
     <div>
       <h2 className='titleHeading'>gas spending</h2>
@@ -11,11 +23,29 @@ export default function App() {
          
         FORM
         <br />
-        <input type="text" placeholder='DATE' />
+        <input 
+        type="number" 
+        placeholder='DATE' 
+        value={dateState}
+        onChange={(e) => setDateState(e.target.value)}
+        />
+        {dateState}{/*  MAY NEED TO SEPARATE INTO Mont Date Year */}
         <br />
-        <input type="text" placeholder='PRICE PER GALLON' />
+        <input 
+        type="text" 
+        placeholder='PRICE PER GALLON'
+        value={priceState}
+        onChange={(e) => setPriceState(e.target.value)}
+        />
+        {priceState}
         <br />
-        <input type="text" placeholder='QT OF GALLONS' />
+        <input 
+        type="text" 
+        placeholder='GALLONS PURCHASED'
+        value={gallonState}
+        onChange={(e) => setGallonState(e.target.value)}
+        />
+        {gallonState}
         <br />
         <button>submit form button</button>
 
