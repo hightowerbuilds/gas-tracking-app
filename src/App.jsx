ba246fd73e1d3fd2eb98ec19b30499305f658f3a
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-16 10:25:31 -0700

    first commit to the hub

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <h2 className='titleHeading'>gas spending</h2>
+
+        <div className='formListContainer'>
+        <div className='formContainer'>
+        
+         
+        FORM
+        <br />
+        <input type="text" placeholder='DATE' />
+        <br />
+        <input type="text" placeholder='PRICE PER GALLON' />
+        <br />
+        <input type="text" placeholder='QT OF GALLONS' />
+        <br />
+        <button>submit form button</button>
+
+
+        </div>
+
+        <div className='listContainer'>
+       
+        LIST
+
+        </div>
+      </div>
+
+     
+    </div>
+  )
+}
