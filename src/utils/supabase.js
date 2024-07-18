ee9dd3c0c49faa9b29f42b45d5bad0ab9fa54d73
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-18 09:08:57 -0700

    more testing

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://pcynpaelrwkgsfmtalwm.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjeW5wYWVscndrZ3NmbXRhbHdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjExNzg3MTcsImV4cCI6MjAzNjc1NDcxN30.9F2-0XRbL0W80p-RRp-_PHrmtznuKJvtCmGe5z5wov4';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
