c700c5823909bcde451eea20e99761f86ec26d60
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-07-19 07:51:20 -0700

    tool bar save

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -12,7 +12,14 @@ export default function ToolBar() {
         <button>data control</button>
         <button>data control</button>
 
-
+ {/**
+  * SO in this component we to enable the user to choose between different pre-set types of sorting ie 
+  * sorting by date, amount of money, gas etc. Basically each of these buttons will change the state of
+  * the DB that we're seeing from unsorted to sorted .... SO how to we go about this? we want to have a 
+  * button that will seek the month and categorize them, it will next look at the day and order them by 
+  * the day of the month, seemingly we can do the same for the other figures (gallons, $)... are we going
+  * to need to make more tables ? possibly creating tables that represent each month?  
+  */}
     </div>
   )
 }
